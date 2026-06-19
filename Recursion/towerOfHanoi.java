cf2cc816b8056e1e74bca38ed69e391ce68435c8
namratasingh99 <namsinghrajpoot9@gmail.com>
2026-06-19 11:23:40 +0530

    Create towerOfHanoi.java

---
+++ Recursion/towerOfHanoi.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class towerOfHanoi {
+    public static void hanoi(int n , char src , char aux , char dest ){
+        if(n==1){
+            System.out.print(src+"-"+dest+" ");
+            return;
+        }
+        hanoi(n-1,src,dest,aux);
+        System.out.print(src+"-"+dest+" ");
+        hanoi(n-1,aux,src,dest);
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of disks");
+        int n = sc.nextInt();
+        hanoi(n,'A' , 'B' , 'C');
+    }
+}
+ 
