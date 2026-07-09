09bae56205291f8dc9f0f8eb3a4195f0ddd3bc79
namratasingh99 <namsinghrajpoot9@gmail.com>
2026-07-09 08:52:22 +0530

    Create MinimumValueInBinaryTree.java

---
+++ BinaryTree/MinimumValueInBinaryTree.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int val){
+        this.data = val;
+        left=right=null;
+    }
+}
+
+public class MinimumValueInBinaryTree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        Node n1 = new Node(100);
+        Node n2 = new Node(12);
+        Node n3 = new Node(3);
+        Node n4 = new Node(9);
+        Node n5 = new Node(5);
+        Node n6 = new Node(36);
+        Node n7 = new Node(7);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        System.out.println(minimum(n1,Integer.MAX_VALUE));
+        
+    }   
+    public static int minimum(Node root , int min){
+        if(root==null) return min;
+        if(root.data<min) min=root.data;
+        return Math.min(minimum(root.left,min),minimum(root.right,min));
+    }
+}
