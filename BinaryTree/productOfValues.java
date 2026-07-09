c6cd0857cd40002488665b5018bcd6b5b29a6e2b
namratasingh99 <namsinghrajpoot9@gmail.com>
2026-07-09 08:51:25 +0530

    Add productOfValues class for binary tree product calculation
    Implement a binary tree and calculate the product of its values.

---
+++ BinaryTree/productOfValues.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int val){
+        this.data = val;
+        left=right=null;
+    }
+}
+
+public class productOfValues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+        Node n7 = new Node(7);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        System.out.println(product(n1));
+        
+    }   
+
+    public static int product (Node root){
+        if(root==null) return 1;               
+        if(root.data==0){
+            return product(root.left)*product(root.right);        //for product of non-zero values
+        }
+        return root.data*product(root.left)*product(root.right);
+    }
+}
